a87ecc6c2c1f8d6a4059d546e3961f04a28a5a74
hy0523 <1159844201@qq.com>
2024-04-23 04:19:22 -0400

    commit

--- visualize.py
+++ visualize.py
@@ -1,14 +1,11 @@
-import sys
-
-sys.path.append('.')
-sys.path.append('..')
 from core.utils.args import Configuration
 from core.manager.visualizer import Visualizer
 import os
 
 config = Configuration()
 config.get_parser()
-config['dataset_name'] = ['DAVIS16']  # ['DAVIS16', 'FBMS-59', 'LongVideos', 'YO2SEG', 'MCL', 'ViSal', 'SegTrack-V2', 'Easy-35']
+config['dataset_name'] = [
+    'DAVIS16']  # ['DAVIS16', 'FBMS-59', 'LongVideos', 'YO2SEG', 'MCL', 'ViSal', 'SegTrack-V2', 'Easy-35']
 config['img_root'] = '../data'
 config['pred_root'] = os.path.join(config['output_dir'], config['model_name'], config['task_name'])
 config['save_root'] = 'qualitative'
